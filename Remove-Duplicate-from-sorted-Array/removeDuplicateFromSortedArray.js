3822861b03e87417070c800e8691775c07880ecb
Emmanuel Donkor <edonkor0000@gmail.com>
2022-12-16 14:33:02 +0100

    Remove Duplicate from sorted array leetcode solution

---
+++ Remove-Duplicate-from-sorted-Array/removeDuplicateFromSortedArray.js
@@ -0,0 +1,19 @@
+var removeDuplicates = function (nums) {
+  //Using the multiple pointers pattern to solve the problem
+  if (nums.length === 0) return 0;
+  let i = 0;
+  for (let j = 1; j < nums.length; j++) {
+    if (nums[i] !== nums[j]) {
+      i++;
+      nums[i] = nums[j];
+    }
+  }
+  return i + 1;
+  /*let i = 0;
+        
+        for (let j = 1; j < nums.length; j++) {
+            if (nums[i] < nums[j]) nums[++i] = nums[j];
+        }
+        
+        return i + 1; */
+};
